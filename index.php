a117170ff0c341dc64ca4ea7b047cf88ec2ac0b4
Thierry-Lopez <titou13530@gmail.com>
2020-07-27 07:39:22 +0200

    creation of css/TLFrame.css file

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 	<head>
 		<meta charset="utf-8">
 		<title>TLFrame Sample</title>
+		<link rel="stylesheet" href="css/TLFrame.css>
+		<script src="js/TLFrame.js"></script>
 	</head>
 	<body id="main">
 		<div id="essai">
