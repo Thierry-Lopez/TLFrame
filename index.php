53db9de9f0afccd0f882e4fd4dbcca84bd72293a
Thierry-Lopez <titou13530@gmail.com>
2020-07-27 07:16:52 +0200

    creation of index.php file

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>TLFrame Sample</title>
+	</head>
+	<body id="main">
+		<div id="essai">
+		
+		</div>
+	</body>
+	<script>
+		
+	</script>
+</html>
