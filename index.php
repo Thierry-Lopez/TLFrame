3b5c817464c0b7e207ebf1b0bfdd139eba734772
Thierry-Lopez <titou13530@gmail.com>
2020-07-29 07:27:53 +0200

    creation of js/class/element.js file

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
 	<head>
 		<meta charset="utf-8">
 		<title>TLFrame Sample</title>
-		<link rel="stylesheet" href="css/TLFrame.css>
+		<link type="text/css" rel="stylesheet" href="css/TLFrame.css" />
 		<script src="js/TLFrame.js"></script>
 	</head>
 	<body id="main">
 		<div id="essai">
-		
+			***
 		</div>
 	</body>
 	<script>
---
+++ js/class/element.js
@@ -0,0 +1,11 @@
+
+	class TLFrameElement {
+		
+		
+	}
+	
+	TLFrame.Element = TLFrameElement;
+		
+		
+	
+	
